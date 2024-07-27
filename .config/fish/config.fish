f5e6c7be14458a9d1b14b0f0356b7f597abd4408
rockshimomime <sh.alencar@rocketmail.com>
2024-07-27 11:32:48 -0300

    chore: add kubectx binary and alias

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,6 +10,8 @@ alias lta="exa --sort Name --long --tree --all"
 alias ls="exa --sort Name --long --all"
 alias k="kubectl"
 alias kgp="kubectl get pods"
+alias kx="kubectx"
+alias kn="kubens"
 alias ping="gping"
 alias cat="bat"
 
